77cf68e34cbc684d8de13c11d4750af8ad2c8b5f
mw29 <madeline.chloe429@gmail.com>
2022-04-26 08:39:35 -0600

    initial commit

---
+++ pong/src/Pong.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './Pong.css'
+
+
+
+function Pong() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1>TEMP</h1>
+    </div>
+  )
+}
+
+export default Pong
